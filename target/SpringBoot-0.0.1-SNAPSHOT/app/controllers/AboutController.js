c011a4fffab2b2a888eeb31223c24db82cd5439a
pradeephbac <pradeephbac@gmail.com>
2016-12-30 15:47:01 +0530

    remove target folder

--- target/SpringBoot-0.0.1-SNAPSHOT/app/controllers/AboutController.js
+++
@@ -1,6 +0,0 @@
-var injectParams = [ '$rootScope', '$scope', '$http' ];
-var AboutController = function($rootScope, $scope, $http) {
-
-};
-AboutController.$inject = injectParams;
-angular.module('controllersModule').controller('AboutController', AboutController);
--- target/SpringBoot-0.0.1-SNAPSHOT/app/controllers/HomeController.js
+++
@@ -1,6 +0,0 @@
-var injectParams = [ '$rootScope', '$scope', '$http' ];
-var HomeController = function($rootScope, $scope, $http) {
-
-};
-HomeController.$inject = injectParams;
-angular.module('controllersModule').controller('HomeController', HomeController);
